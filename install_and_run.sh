25b04401869daa93d9afd544f626ce81c8a492a6
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-09-10 06:54:54 +0200

    2c install in home

--- install_and_run.sh
+++ install_and_run.sh
@@ -4,13 +4,14 @@ pip install triton
 pip install cmake
 
 # Folder where DNABERT2 will be installed in
-DNABERT_2_SUPERFOLDER=/proj/staff/richel
+# DNABERT_2_SUPERFOLDER=/proj/staff/richel
+DNABERT_2_SUPERFOLDER=/home/richel
 
-{
+(
   cd "${DNABERT_2_SUPERFOLDER}" || exit
   git clone https://github.com/MAGICS-LAB/DNABERT_2
   cd DNABERT_2 || exit
   python3 -m pip install -r requirements.txt
-}
+)
 
 ./run.sh
